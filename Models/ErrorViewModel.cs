20cea3f2c1faf0b448e74998fc1df74530586559
Kevin Sebastian Lee <48044115@est.ort.edu.ar>
2024-07-18 19:12:44 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_05_Lee_Suez_Berenstein.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
