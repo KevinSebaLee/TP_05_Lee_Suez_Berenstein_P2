a35a66c8173bf14d2dd5eb58b0f1dfbdd0408745
KevinSebaLee <48044115@est.ort.edu.ar>
2024-07-22 21:59:39 -0300

    Link accesible para el telefono y mail en el Index.cshtml

--- obj/Debug/net8.0/TP_05_Lee_Suez_Berenstein.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP_05_Lee_Suez_Berenstein.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_05_Lee_Suez_Berenstein")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ba8a102f2d2ec7e4c686e45c03e6f763cb91c6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b1193bb85bf8956c65e818b1492a01efa95e02")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_05_Lee_Suez_Berenstein")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_05_Lee_Suez_Berenstein")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
